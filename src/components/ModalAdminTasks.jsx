50a3262ab907364daeee540b5b41c17a22b7f5f3
LexyIKO <124718101+LexyIKO@users.noreply.github.com>
2024-04-04 22:24:32 +0300

    add admin tasks panel

--- src/components/ModalAdminTasks.jsx
+++ src/components/ModalAdminTasks.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Text, SafeAreaView, View, TextInput, FlatList, Pressable, Modal, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native';
 import MiniTaskBox from './MiniTaskBox';
 import HeaderForModal from './HeaderForModal'
 import OneTaskModal from './OneTaskModal';
 
-const ModalDefaultTasks = (props) => {
+const ModalAdminTasks = (props) => {
  
     const [taskList, setTaskList] = useState([]);
 
@@ -15,36 +16,20 @@ const ModalDefaultTasks = (props) => {
     };
 
     let TempTaskList = [       
-        {id: 0, key: 'lalala', title: 'hello world', description: 'Выйти ночью в поле с конем, ночкой темной тихо вдвоем', time: 'April 17, 2024 00:00:00', isCompleted: true},
-        {id: 1, key: 'hahaha', title: 'My name uwewewe onit uwewe', description: 'Посмотреть где можно закупиться по скидке, да так, чтобы еще на послезавтра деньги остались', time: '2024, 10, 2', isCompleted: false},
-        {id: 2, key: 'blabla', title: 'Мне сегодня 7 лет', description: 'Пожевать жувачку', time: '12/06/2024', isCompleted: false},
+        {id: 0, key: 'lalala', title: 'Красавица и чудовище', description: 'Помочь двум людям, один из должен вам нравиться, другой - нет', time: 'April 17, 2024 00:00:00', isCompleted: true},
+        {id: 1, key: 'hahaha', title: 'Короли болота', description: 'Уберитесь у себя дома, пожалуйста, наконце-то уже!', time: '2024, 10, 2', isCompleted: false},
+        {id: 2, key: 'blabla', title: 'Речное чудище', description: 'Сходи в душ, помой голову, уложи волосы, не позорься!', time: '12/06/2024', isCompleted: false},
     ]
 
     const getTaskList = () =>{
-
+        
         //toDo
         setTaskList(TempTaskList)
     };
 
-
-
-    const sortTaskList = () => {
-        for (i in taskList) {
-            console.log()
-        }
-    };
-
-    const changeTaskStatus = (taskID) => {
-        //toDo
-        getTaskList();
-    };
-    
-
-    
     useEffect(() => {
         getTaskList()
     }, [])
-
 
     const [CurrentTask, SetCurrentTask] = useState({});
 
@@ -62,7 +47,7 @@ const ModalDefaultTasks = (props) => {
             visible={props.visability}
             onRequestClose={closeModal}
         >
-            <HeaderForModal fontSize = {{fontSize: 24}} onModalClose = {closeModal} title='Обычные задачи'/>
+            <HeaderForModal fontSize = {{fontSize: 24}} onModalClose = {closeModal} title='Задачи от трации'/>
 
             <View style = {styles.container}>
                 <FlatList
@@ -71,21 +56,13 @@ const ModalDefaultTasks = (props) => {
                 style = {styles.flatlist}
                 
                 />
-
-                <Pressable style = {{width: 100, height: 100, backgroundColor: 'red'}}
-                onPress = {() => sortTaskList()}>
-                    <Text>Push</Text>
-                </Pressable>
             </View>
 
             <OneTaskModal 
                 visibility={OneTaskVisability} 
-                onCloseModal={() => SetOneTaskVisible(false)}
-                onStatusChanged= {()=>changeTaskStatus(CurrentTask.id)} 
+                onCloseModal={() => SetOneTaskVisible(false)} 
                 item = {CurrentTask}
             />
-
-            
 
         </Modal>
     );
@@ -103,4 +80,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ModalDefaultTasks;
+export default ModalAdminTasks;
